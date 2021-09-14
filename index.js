b2d8ea25dbf1b8d06e65721030a4f8421f4711e1
Pham Tran Duc Trong <trong.ptd3604@sinhvien.hoasen.edu.vn>
2021-09-14 09:17:30 +0700

    wakeup server

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ server.get('/', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/store/index.html'));
 });
 
+(function wakeup() {
+  require('open')('https://duckstore.herokuapp.com/', (err) => {
+    if (err) throw err;
+    console.log('Woke up!');
+    setTimeout(wakeup, 1740000); //29m
+  });
+})()
+
 server.listen(process.env.PORT || 3000, function(){
     console.log("Your app running on port 3000");
 })
